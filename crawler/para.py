b5aee205d1c51c1b3f3f71adb2e163ba54f395b0
Elen <elenflores02@gmail.com>
2016-03-09 07:18:41 +0800

    crawled a website

--- crawler/para.py
+++ crawler/para.py
@@ -2,15 +2,20 @@ from bs4 import BeautifulSoup
 
 content = open("461216.html",'r').read()
 soup = BeautifulSoup(content, 'html.parser')
+title = soup.findAll('h1',{'class':'title'})
+caption = soup.findAll('div',{'class':'image-caption clearfix'})
 result = soup.findAll('div',{'class':'field-item even'})
 
 f = open('461216.txt', 'w')
+f.write(str(title))
+f.write(str(caption))
 f.write(str(result))
 f.close()
 
 content2 = open("461216.txt",'r').read()
 soup2 = BeautifulSoup(content2, 'html.parser')
 result2 = soup2.get_text()
+
 
 f = open('461216.txt', 'w')
 f.write(str(result2))
